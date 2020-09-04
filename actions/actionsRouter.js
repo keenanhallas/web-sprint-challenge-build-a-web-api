f236f01e8715e0ededa32790692ca90f3196ec89
Keenan <keenanhallas@gmail.com>
2020-09-04 12:13:40 -0500

    all endpoints complete & functional

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -16,22 +16,47 @@ router.get("/", (req, res) => {
 
 //Get a specific action
 router.get("/:id", (req, res) => {
-    
+    actionsDb.get(req.params.id)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 //Post an action
+//is it better to somehow get the project id from the url?
 router.post("/", (req, res) => {
-
+    actionsDb.insert(req.body)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 //Update a specific action
 router.put("/:id", (req, res) => {
-    
+    actionsDb.update(req.params.id, req.body)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 //Delete a specific action
 router.delete("/:id", (req, res) => {
-    
+    actionsDb.remove(req.params.id)
+        .then(response => {
+            res.status(204).end();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 module.exports = router;
